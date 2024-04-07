3053e98275399609bdc845b97b08d669a2068974
dir-FY24matsushita <momoka.matsushita.g090@dirbato.co.jp>
2024-04-07 21:22:53 +0900

    割り算とキャスト

--- Sample/src/sample/Cast.java
+++ Sample/src/sample/Cast.java
@@ -8,6 +8,11 @@ public class Cast {
 	System.out.println(a/b);
 	//小数点以下は切り捨てになった
 	
+	System.out.println((double)a/b);
+	//これはaをdouble型にキャストしてから割り算。つまり３．０/２＝１．５と同じ。
+	
+	System.out.println((double)(a/b));
+	//これはa/bの答えをdouble型にキャストしている
 	
 	
 	
