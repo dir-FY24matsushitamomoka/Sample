a8000c2fe129d6841c983fdaa04e34ddfaa83576
dir-FY24matsushita <momoka.matsushita.g090@dirbato.co.jp>
2024-04-08 16:36:57 +0900

    If文の練習

--- Sample/src/sample/IfSample.java
+++ Sample/src/sample/IfSample.java
@@ -14,8 +14,33 @@ public class IfSample {
 	}
 	//60点以上の条件のため、falseとなり結果は表示されない
 	
+	score =-60; //成績
+	if (score>=60) {
+		System.out.println("合格です！");
+		
+	}
+	else {
+		System.out.println("赤点です。。。");
+	}
 	
+	score=100 ;
+	if (score==100) {
+		System.out.println("満点です！");
+		
+	}
+	else if (score>=80) {
+		System.out.println("よくできました！");
+		
+	}
+	else if (score>=60){
+		System.out.println("合格です！");
+		
+	}
+	else {
+		System.out.println("赤点です。。。");
+	}
+	
+	}
 
 	}
 
-}
