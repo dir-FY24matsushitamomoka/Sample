00d65ed8a7ce352e41e621f5ca354dbe642dc85c
dir-FY24matsushita <momoka.matsushita.g090@dirbato.co.jp>
2024-04-12 09:27:22 +0900

    Junitの練習

---
+++ Sample/src/sample/Calculator.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class Calculator {
+	
+	public class Calculator{
+	
+		public int add (int a,int b) {
+			return a+b;
+			
+		}
+		
+		public int substracy (int a, int b) {
+			return a-b;
+			
+		}
+	}
+
+}
